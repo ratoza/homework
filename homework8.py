b0bd1a1edc3fc17e61d7b197ff63ac3a68d8bc7d
ratoza <54843289+ratoza@users.noreply.github.com>
2019-12-10 20:04:01 +0100

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,20 @@
+print("This program asks for a number and tests if it is prime")
+while True:
+    try:
+        read_line = input("Please give me an integer number")
+        num = int(read_line)
+    except ValueError:
+        print("That was not a valid integer number")
+        continue
+    break
+
+count = 0
+while count < 100:
+    for i in range(2, num // 2):
+        if num % i == 0:
+            print("The number", num, "is not prime")
+            num = num + 1
+            count = count + 1
+    print("The number", num, "is prime")
+    num = num+1
+    count = count+1
